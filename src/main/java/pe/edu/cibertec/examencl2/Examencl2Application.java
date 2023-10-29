edb8cd906fcd531b1b95b3eb39ba675abcd9a9b1
JuniorM2702 <Junior_aybar27@hotmail.com>
2023-10-29 10:27:28 -0500

    primer commit

---
+++ src/main/java/pe/edu/cibertec/examencl2/Examencl2Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.examencl2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Examencl2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Examencl2Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/examencl2/Examencl2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.examencl2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Examencl2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
